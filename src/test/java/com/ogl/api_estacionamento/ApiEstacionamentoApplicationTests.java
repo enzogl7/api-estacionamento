6c2ae16c04e01d96b078bfc995f4e1f347c754a6
enzogl7 <enzolima527@gmail.com>
2025-06-18 14:43:01 -0300

    Initial commit

---
+++ src/test/java/com/ogl/api_estacionamento/ApiEstacionamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ogl.api_estacionamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiEstacionamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
